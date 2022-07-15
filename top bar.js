377418f678e20b8f1ed187475ddc17af410ea8cc
unknown <momen07996@gmail.com>
2022-07-15 05:12:05 -0700

    top bar

---
+++ top bar.js
@@ -0,0 +1,53 @@
+var click = function () {
+
+var h1 = document.getElementById("h1");
+
+h1.style.display = "inline-block";
+}
+
+document.getElementById("p").onclick = click;
+
+var click1 = function () {
+
+var h2 = document.getElementById("h2");
+
+h2.style.display = "inline-block";
+}
+
+document.getElementById("p1").onclick = click1;
+
+var click2 = function () {
+
+var h3 = document.getElementById("h3");
+
+h3.style.display = "inline-block";
+}
+
+document.getElementById("p2").onclick = click2;
+
+var click3 = function () {
+
+var h4 = document.getElementById("h4");
+
+h4.style.display = "inline-block";
+}
+
+document.getElementById("p3").onclick = click3;
+
+var click4 = function () {
+
+var h5 = document.getElementById("h5");
+
+h5.style.display = "inline-block";
+}
+
+document.getElementById("p4").onclick = click4;
+
+var click5 = function () {
+
+var h6 = document.getElementById("h6");
+
+h6.style.display = "inline-block";
+}
+
+document.getElementById("p5").onclick = click5;
